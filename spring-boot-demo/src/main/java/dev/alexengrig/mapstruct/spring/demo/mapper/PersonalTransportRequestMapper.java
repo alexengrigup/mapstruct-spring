adec4a224e5af5e1cb04d2138c7161acacb03d15
Grig Alex <alexengrigdev@gmail.com>
2023-03-16 23:43:36 +0300

    Update PersonalTransportRequestMapper

--- spring-boot-demo/src/main/java/dev/alexengrig/mapstruct/spring/demo/mapper/PersonalTransportRequestMapper.java
+++ spring-boot-demo/src/main/java/dev/alexengrig/mapstruct/spring/demo/mapper/PersonalTransportRequestMapper.java
@@ -1,18 +1,18 @@
 package dev.alexengrig.mapstruct.spring.demo.mapper;
 
 import dev.alexengrig.mapstruct.spring.config.SpringMapStructConfig;
+import dev.alexengrig.mapstruct.spring.demo.converter.UserIdConverter;
 import dev.alexengrig.mapstruct.spring.demo.domain.PersonalTransport;
 import dev.alexengrig.mapstruct.spring.demo.dto.PersonalTransportRequest;
 import dev.alexengrig.mapstruct.spring.mapper.RequestMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(config = SpringMapStructConfig.class)
+@Mapper(config = SpringMapStructConfig.class, uses = UserIdConverter.class)
 public interface PersonalTransportRequestMapper
         extends RequestMapper<PersonalTransportRequest, PersonalTransport> {
 
-    @Mapping(target = "owner", ignore = true)
-    @Mapping(target = "owner.id", source = "ownerId")
+    @Mapping(target = "owner", source = "ownerId")
     @Override
     PersonalTransport mapRequestToDomain(PersonalTransportRequest personalTransportRequest);
 
